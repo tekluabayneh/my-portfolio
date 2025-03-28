c834438a1c44f8f24d500cd67bfcdde88de1d1b4
teklu abayneh <tekluabayneh@gmail.com>
2025-03-28 16:07:52 +0300

    Modified src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import "./mainStyle.css";
 import AppRouter from "./Router";
+
 function App() {
   return (
     <main className="bg-[#e8e9e8] scroll-smooth font-poppins overflow-hidden  overflow-x-scroll snap-x snap-mandatory scroll-smooth">
